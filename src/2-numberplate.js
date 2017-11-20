660d80c7bae6f564bf9cca7c43c8019bbfb1fd43
LorenzoLim <renzo-lim@hotmail.com>
2017-11-20 12:01:02 +1000

    2-numberplate.js Added

--- src/2-numberplate.js
+++ src/2-numberplate.js
@@ -1,14 +1,33 @@
 function formatVanityPlate(string) {
-  let words = string.split(' ')
-  words = words.map(word => word.toUpperCase())
-  return words.join('')
+let wordsLength = string.split(' ')
+let stringLength = wordsLength.join('')
+  if(stringLength.length <= 12){
+    let words = string.split(' ')
+    words.forEach(function(word,index){
+      if (word === 'for'){
+        words[index] = '4'
+      }else if (word === 'to'){
+        words[index] = '2'
+      }
+    });
+    words = words.map(word => word.toUpperCase())
+    return words.join('')
+  }else{
+    return 'Number Plate Too Long!'
+  }
 }
 
 const one = 'I like money'
 console.log(`Number plate: ${ formatVanityPlate(one) }`)
 
-const two = 'Cats for life'
+const two = 'Born to dab'
 console.log(`Number plate: ${ formatVanityPlate(two) }`)
+
+const three = 'Cats for life'
+console.log(`Number plate: ${ formatVanityPlate(three) }`)
+
+const four = 'Cats for life but not dogs because they suck'
+console.log(`Number plate: ${ formatVanityPlate(four) }`)
 
 
 /*
