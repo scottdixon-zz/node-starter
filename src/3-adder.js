9d7a273f1cfa1cb848e58f67ccba29b3f81837d6
LorenzoLim <renzo-lim@hotmail.com>
2017-11-20 12:21:47 +1000

    3-adder.js Added

--- src/3-adder.js
+++ src/3-adder.js
@@ -6,9 +6,27 @@ function sum(numbers) {
   return total
 }
 
-console.log(sum([12, 5])) // => 17
-console.log(sum([5, 7, 1])) // => 13
+function multiply(numbers) {
+  let total = 1
+  numbers.forEach(number => {
+    total = total * number
+  })
+  return total
+}
 
+console.log(`The 12 + 5 = ${sum([12, 5])}`) // => 17
+console.log(`The 5 + 7 + 1 = ${sum([5, 7, 1])}`) // => 13
+console.log(`The 12 x 5 = ${multiply([12,5])}`) // => 60
+console.log(`The 5 x 7 x 1 = ${multiply([5,7,1])}`) // => 35
+
+
+function multiplyNumbersString(numbersString) {
+  const numbers = numbersString
+    .split('x')
+    .map(string => parseFloat(string))
+
+  return multiply(numbers)
+}
 
 function sumNumbersString(numbersString) {
   const numbers = numbersString
@@ -19,9 +37,13 @@ function sumNumbersString(numbersString) {
 }
 
 
-console.log(sumNumbersString('12 + 5')) // => 17
-console.log(sumNumbersString('5 + 7 + 1')) // => 13
-console.log(sumNumbersString('1 + 2 + 3 + 4 + 5')) // => 15
+console.log(`The 12 + 5 = ${sumNumbersString('12 + 5')}`) // => 17
+console.log(`The 5 + 7 + 1 = ${sumNumbersString('5 + 7 + 1')}`) // => 13
+console.log(`The 1 + 2 + 3 + 4 + 5 = ${sumNumbersString('1 + 2 + 3 + 4 + 5')}`) // => 15
+
+console.log(`The 12 x 5 = ${multiplyNumbersString('12 x 5')}`) // => 17
+console.log(`The 5 x 7 x 1 = ${multiplyNumbersString('5 x 7 x 1')}`) // => 13
+console.log(`The 1 x 2 x 3 x 4 x 5 = ${multiplyNumbersString('1 x 2 x 3 x 4 x 5')}`) // => 15
 
 
 /*
