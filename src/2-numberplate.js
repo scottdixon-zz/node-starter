b85ef189f396f0f4bd1374c1af8ad47ed0517dcb
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-11-20 14:52:47 +1000

    added solution to 2-numberplate.js using npm prompt package

--- src/2-numberplate.js
+++ src/2-numberplate.js
@@ -1,15 +1,62 @@
+const prompt = require('prompt');
+
 function formatVanityPlate(string) {
   let words = string.split(' ')
   words = words.map(word => word.toUpperCase())
   return words.join('')
 }
 
+numberPlates = []
+
 const one = 'I like money'
-console.log(`Number plate: ${ formatVanityPlate(one) }`)
+numberPlates.push(formatVanityPlate(one))
 
 const two = 'Cats for life'
-console.log(`Number plate: ${ formatVanityPlate(two) }`)
+numberPlates.push(formatVanityPlate(two.replace('for', '4')))
 
+const three = 'Born to Ride'
+numberPlates.push(formatVanityPlate(three.replace('to', '2')))
+
+
+properties = [
+    {
+      name: 'plate',
+      message: 'Enter a vanity plate name (Enter the word done when you have finished)',
+      maxLength: 12,
+      required: true,
+      warning: 'Vanity plate name must be less than 12 characters'
+    }
+  ];
+
+prompt.start()
+
+function enterPlate() {
+  prompt.get(properties, function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('  You have entered: ' + result.plate)
+    if (result.plate !== 'done') {
+      numberPlates.push(formatVanityPlate((result.plate.replace('to', '2')).replace('for', '4')))
+    }
+    finishPrompt = result.plate
+    if (finishPrompt === 'done') {
+      console.log('Thanks')
+      console.log('Here are all the registered Vanity Plates: ')
+      numberPlates.forEach(function(plate) {
+        console.log('- ' + plate)
+      })
+      console.log('Have a nice day! :)')
+    } else {
+      enterPlate()
+    }
+  });
+
+  function onErr(err) {
+    console.log(err);
+    return 1;
+  }
+}
+
+enterPlate()
 
 /*
 
