d21caed62c9a65fbf20d86d3a61530904782a006
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-11-20 11:55:16 +1000

    Celsius

--- src/1-converter.js
+++ src/1-converter.js
@@ -1,14 +1,22 @@
 function convertCelsiusToFahrenheit(celsius) {
   return (celsius * 1.8) + 32
-}
+};
 
-let fahrenheit
+function convertFahrenheitToCelsius(fahrenheit) {
+  return (fahrenheit - 32) / 1.8
+};
+
+function formatCelsius(value) {
+    return (Math.round(value) + '°C');
+};
 
 // Monday
-
-const mondayTemperature = 24
-console.log(`On Monday it is ${mondayTemperature}°C`)
+const mondayTemperature = 100 //fahrenheit
+//Test convertFahrenheitToCelsius function
+let celsius = formatCelsius(convertFahrenheitToCelsius(mondayTemperature))
+console.log(`On Monday it is ${celsius} `)
 fahrenheit = convertCelsiusToFahrenheit(mondayTemperature)
+
 console.log(`The Americans would think of that as ${fahrenheit}°F`)
 
 // Tuesday
@@ -18,10 +26,17 @@ console.log(`On Tuesday it is ${tuesdayTemperature}°C`)
 fahrenheit = convertCelsiusToFahrenheit(tuesdayTemperature)
 console.log(`The Americans would think of that as ${fahrenheit}°F`)
 
+
+// Wednesday
+const wednesdayTemperature = 31
+console.log(`On Wednesday it is ${wednesdayTemperature}°C`)
+fahrenheit = convertCelsiusToFahrenheit(wednesdayTemperature)
+console.log(`The Americans would think of that as ${fahrenheit}°F`)
+
 /*
 
 Challenges:
-1. Add Fahrenheit to Celsius converter (`function convertFahrenheitToCelsius`) with examples of usage
+1. Add Fahrenheit to Celsius converter (`function convertFahrenheitToCelsius`) with examples of  Done
 2. Add `formatCelsius` function that rounds to whole number using `Math.round`, and adds '°C' on end
 3. Add `hottestTemperature` function, which takes an array of temperatures, and returns the highest one
 
