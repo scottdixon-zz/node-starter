9ec5137d226a79ae612eaaa0f21cd1f09787d5c9
LorenzoLim <renzo-lim@hotmail.com>
2017-11-20 11:44:17 +1000

    1-converter.js Completed

--- src/1-converter.js
+++ src/1-converter.js
@@ -2,21 +2,52 @@ function convertCelsiusToFahrenheit(celsius) {
   return (celsius * 1.8) + 32
 }
 
+function convertFahrenheitToCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 0.5556
+}
+
+function formatCelsius(celsius){
+  let roundedVal = Math.round(celsius)
+  return roundedVal + '°C'
+}
+
+function formatFahrenheit(fahrenheit){
+  let roundedVal = Math.round(fahrenheit)
+  return roundedVal + '°F'
+}
+
+function hottestTemperature(temps){
+var hottestTemp = 0;
+  for (var i = 0; i < temps.length; i++) {
+    if (hottestTemp < temps[i]){
+      hottestTemp = temps[i];
+    }
+  }
+return hottestTemp
+}
+
+temps = [24,26,23,30];
+console.log(`${hottestTemperature(temps)}°C is the hottest temperature`)
+
 let fahrenheit
+let celsius
 
 // Monday
 
-const mondayTemperature = 24
-console.log(`On Monday it is ${mondayTemperature}°C`)
-fahrenheit = convertCelsiusToFahrenheit(mondayTemperature)
-console.log(`The Americans would think of that as ${fahrenheit}°F`)
+const mondayCelciusTemperature = 24
+const mondayFarenheitTemperature = 75
+
+celsius = formatCelsius(convertFahrenheitToCelsius(mondayFarenheitTemperature))
+console.log(`On Monday it is ${celsius}`)
+fahrenheit = formatFahrenheit(convertCelsiusToFahrenheit(mondayCelciusTemperature))
+console.log(`The Americans would think of that as ${fahrenheit}`)
 
 // Tuesday
 
 const tuesdayTemperature = 31
 console.log(`On Tuesday it is ${tuesdayTemperature}°C`)
-fahrenheit = convertCelsiusToFahrenheit(tuesdayTemperature)
-console.log(`The Americans would think of that as ${fahrenheit}°F`)
+fahrenheit = formatFahrenheit(convertCelsiusToFahrenheit(tuesdayTemperature))
+console.log(`The Americans would think of that as ${fahrenheit}`)
 
 /*
 
