8f26b9e8f90a7929de1f5226586a6f3b03e769a0
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-11-20 12:49:00 +1000

    Added solutions to 1-converter

--- src/1-converter.js
+++ src/1-converter.js
@@ -2,7 +2,19 @@ function convertCelsiusToFahrenheit(celsius) {
   return (celsius * 1.8) + 32
 }
 
+function convertFahrenheitToCelsius(fahrenheit) {
+  return (fahrenheit - 32) / 1.8
+}
+
+function formatCelsius(temp) {
+  return `${Math.round(temp)}°C`
+}
+
 let fahrenheit
+
+let celsius
+
+let temps = []
 
 // Monday
 
@@ -10,6 +22,7 @@ const mondayTemperature = 24
 console.log(`On Monday it is ${mondayTemperature}°C`)
 fahrenheit = convertCelsiusToFahrenheit(mondayTemperature)
 console.log(`The Americans would think of that as ${fahrenheit}°F`)
+temps.push(mondayTemperature)
 
 // Tuesday
 
@@ -17,6 +30,22 @@ const tuesdayTemperature = 31
 console.log(`On Tuesday it is ${tuesdayTemperature}°C`)
 fahrenheit = convertCelsiusToFahrenheit(tuesdayTemperature)
 console.log(`The Americans would think of that as ${fahrenheit}°F`)
+temps.push(tuesdayTemperature)
+
+// Wednesday
+
+const wednesdayTemperature = 109
+console.log(`On Wednesday it is ${wednesdayTemperature}°F`)
+celsius = convertFahrenheitToCelsius(wednesdayTemperature)
+console.log(`The Europeans would think of that as ${formatCelsius(celsius)}`)
+temps.push(Math.round(celsius))
+
+// Weekly high
+const hottestTemperature = (temps) => {
+  return Math.max(...temps)
+}
+
+console.log(`The hottest temperature this week is ${hottestTemperature(temps)}°C`)
 
 /*
 
